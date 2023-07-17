032fab9f757006babed7b1a6868969e38c57002b
_NineLota_ <90235641+ImAFrogOwO@users.noreply.github.com>
2023-07-17 03:23:42 -0500

    Create Enchance.cs

---
+++ Outpath-Modding/Outpath-Modding/GameConsole/Commands/Enchance.cs
@@ -0,0 +1,33 @@
+﻿using Outpath_Modding.GameConsole.Components;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Outpath_Modding.GameConsole.Commands
+{
+    public class Enhance : ICommand
+    {
+        public string Command { get; set; } = "enhance";
+        public string[] Abbreviate { get; set; } = new string[] { };
+        public string Description { get; set; } = "Graphics go brrrrr";
+
+        private RLProColormapPalette[] Color;
+
+        public bool Execute(string[] args, out string reply)
+        {
+            if (args == null || args.Length <= 0)
+            {
+                string name = SceneManager.GetActiveScene().name;
+          			bool flag = name == "Scene_Game";
+          			if (flag)
+          			{
+          				this.Color = Object.FindObjectsOfType<RLProColormapPalette>();
+          				foreach (RLProColormapPalette rlproColormapPalette in this.Color)
+          				{
+          					rlproColormapPalette.resolution.value = new Vector2Int(40000, 40000);
+          				}
+          			}
+                return true;
+            }
+        }
+    }
+}
